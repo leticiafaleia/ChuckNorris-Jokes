0e4c67f8678c94d062eddba77617752aa96a5b62
Letícia Santos <leticia.faleia@gmail.com>
2020-08-24 16:34:17 -0300

    Criação do projeto

---
+++ Fatos CN/Fatos CN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fatos CN
+//
+//  Created by Letícia Santos on 24/08/20.
+//  Copyright © 2020 Letícia Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
