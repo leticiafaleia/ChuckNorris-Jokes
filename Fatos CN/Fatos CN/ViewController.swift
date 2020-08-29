d9eac00be65ba1b497666436ebade8f1e0330e33
Letícia Santos <leticia.faleia@gmail.com>
2020-08-29 18:12:17 -0300

    Pausa

--- Fatos CN/Fatos CN/ViewController.swift
+++ Fatos CN/Fatos CN/ViewController.swift
@@ -21,10 +21,23 @@ struct Facts: Decodable {
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var jokeLabel: UILabel!
-    @IBAction func nextBtn(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         fetchJokes()
     }
+    
+    var favoritesJokes = [String]()
+    
+    @IBOutlet weak var jokeLabel: UILabel!
+    @IBAction func favoriteBtn(_ sender: Any) {
+        favoritesJokes.append(jokeLabel.text ?? "")
+        print("Favoritado")
+    }
+    @IBAction func nextBtn(_ sender: Any) {
+        fetchJokes()
+        print("Próxima piada")
+    }
+    
     @IBAction func shareJoke(_ sender: Any) {
         let shareBtn = UIActivityViewController(activityItems: [jokeLabel.text], applicationActivities: nil)
         
@@ -42,10 +55,6 @@ class ViewController: UIViewController {
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        fetchJokes()
-    }
         
     func fetchJokes(){
         let url = "https://api.chucknorris.io/jokes/random"
@@ -54,7 +63,7 @@ class ViewController: UIViewController {
             switch response.result {
             case .success:
                 DispatchQueue.main.async {
-                    self.jokeLabel.text = value.text
+                    self.jokeLabel?.text = value.text
                 }
             case .failure(let error):
                 print("Error to fetch data! \(error)")
