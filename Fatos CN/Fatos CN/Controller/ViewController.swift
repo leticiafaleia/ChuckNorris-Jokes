21907d92bf43cc5fe40a6ac58a9dbdb6e43e3822
Letícia Santos <leticia.faleia@gmail.com>
2020-09-05 17:29:32 -0300

    Carrega piadas favoritadas

--- Fatos CN/Fatos CN/Controller/ViewController.swift
+++ Fatos CN/Fatos CN/Controller/ViewController.swift
@@ -7,36 +7,58 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+
+    var favoritesJokes: [NSManagedObject] = []
     
     @IBOutlet weak var jokeLabel: UILabel!
-
-    var favoriteJokes = [String]()
-
+    @IBOutlet weak var tableView: UITableView!
+    
+// MARK - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        jokeLabel.text = "loading joke..."
+        jokeLabel?.text = "loading joke..."
         loadJokes()
     }
     
-    @IBAction func favoriteBtn(_ sender: UIButton) {
-        var joke: Joke!
-        joke = Joke.init(context: context)
-        joke.quote = jokeLabel.text
-        do {
-            try context.save()
-            print("save into favs list")
-        } catch {
-            print(error.localizedDescription)
+// MARK - Fetch Data from Core Data
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+
+      guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+          return
+      }
+      
+      let managedContext = appDelegate.persistentContainer.viewContext
+      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Joke")
+
+      do {
+        favoritesJokes = try managedContext.fetch(fetchRequest)
+      } catch let error as NSError {
+        print("Could not fetch. \(error), \(error.userInfo)")
+      }
+    }
+
+    
+// MARK - Fetch Jokes from the API
+    func loadJokes(){
+        ServiceAPI.fetchJokes { (info) in
+            if let info = info {
+                self.jokeLabel?.text = info.text
+            }
         }
     }
-    
+// MARK - Next Joke Button
     @IBAction func nextBtn(_ sender: Any) {
         print("next joke")
         loadJokes()
     }
     
+    
+// MARK - Share Joke button
     @IBAction func shareJoke(_ sender: Any) {
         let shareBtn = UIActivityViewController(activityItems: [jokeLabel.text], applicationActivities: nil)
         
@@ -47,17 +69,53 @@ class ViewController: UIViewController {
                 print("canceled")
             }
         }
-        
         present(shareBtn, animated: true){
             print("presented")
         }
     }
     
-    func loadJokes(){
-        ServiceAPI.fetchJokes { (info) in
-            if let info = info {
-                self.jokeLabel.text = info.text
-            }
+// MARK - Save Joke
+    @IBAction func saveJoke(_ sender: UIButton) {
+        
+        guard let jokeText = jokeLabel?.text else {
+                return
+        }
+        self.save(quote: jokeText)
+        self.tableView?.reloadData()
+    }
+    
+    func save(quote: String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+           return
+         }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Joke", in: managedContext)!
+        let joke = NSManagedObject(entity: entity, insertInto: managedContext)
+        joke.setValue(quote, forKeyPath: "quote")
+        
+        do {
+          try managedContext.save()
+            favoritesJokes.append(joke)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
         }
     }
+}
+
+
+// MARK - TableView
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return favoritesJokes.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    let joke = favoritesJokes[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = joke.value(forKeyPath: "quote") as? String
+    return cell
+  }
 }
